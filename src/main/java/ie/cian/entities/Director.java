cd3d3f3329ea296cf779113a4b48c2dfa3f1c29c
Cian O'Sullivan <cjp1805@outlook.com>
2020-12-11 23:45:03 +0000

    Added Directors Name Varaible which is required to prevent a crash on director

--- src/main/java/ie/cian/entities/Director.java
+++ src/main/java/ie/cian/entities/Director.java
@@ -33,21 +33,18 @@ public class Director {
 	@Column(nullable = false, unique = true)
 	private String surname;
 	
-	// One County has many towns
-	// When fetching a County from a query, do not fetch the list of towns. 
-	// If the county is removed from the database, also remove the towns. 
-	// One county to many towns
-	// Mapped by is specifically saying what field to use in town
-	// Use the fetch type field, lazy means when getting county
-	// information, dont get the twon information at the same
-	// time. It will rely on you getting the town information.
-	// Saves on resources if you dont need all of the town 
-	// Cascade means if a county is deleted to remove all towns in that county
+	/*
+	 * For some reason directorName is needed, don't know why but program doesn't work without it
+	 */
+	@Column(nullable = false, unique = true)
+	private String directorName;
+	
 	@OneToMany(mappedBy = "filmDirector", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
 	private List<Film> directorFilms = new ArrayList<>();
 
 	public Director(String fn, String sm) {
 		this.firstname = fn;
 		this.surname = sm;
+		this.directorName = fn + " " + sm;
 	}
 }
