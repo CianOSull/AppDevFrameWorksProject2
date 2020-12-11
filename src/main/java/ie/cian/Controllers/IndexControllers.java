df2ab020334451bba283bb24823a0af65366f416
Cian O'Sullivan <cjp1805@outlook.com>
2020-12-11 23:04:25 +0000

    Fixed fragment not displaying right name for new film

--- src/main/java/ie/cian/Controllers/IndexControllers.java
+++ src/main/java/ie/cian/Controllers/IndexControllers.java
@@ -7,57 +7,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-// Indicates that it is a controller
-// Serves a specialization of component
-// Flags that everything in here is a controller
 @Controller
 public class IndexControllers {
-	
-	// What this is doing is that if you type just localhost8080
-	// or just localhost8080/, this method will be called.
-	// In the background Thymleaf, will know to put  .hmtl
-	// at the end of the string returned.
-	// This handles get requests
-	/*
-	 * Request Param:
-	 * localhost8080?myname=Cian
-	 * This handles getting data into the controller. 
-	 * It handles request parameters.
-	 * The model is the way to get data from the controller to the view
-	 * M in MVC stands for model
-	 * Example url: http://localhost:8080/?myname=Cian&myemail=cian.osullivan4@mycit.ie
-	 * 
-	 * Required means you dont need to input a valaue and will isntead use default
-	 */
+	// For now just going to have the index do nothing
+	// As it just needs to be a welcome page for now
+	// But leaving this function her just in case for later
 	@GetMapping("/")
-	public String loadIndexGet(
-			@RequestParam(name = "myname", required=false, defaultValue = "") String name, 
-			@RequestParam(name = "myemail", required=false, defaultValue = "") String email, 
-			Model model)
-	{
-		// This is now going to send the persons request with the value
-		model.addAttribute("name", name);
-		model.addAttribute("email", email);
-		return "index";
-	}
-	
-	// This handles if a value is in the path e.g. 
-	// http://localhost:8080/details/Cian/cian.osullivan4@mycit.ie
-	@GetMapping("/details/{myname}/{myemail}")
-	public String loadIndexGetPath(@PathVariable("myname") String name,
-			@PathVariable("myemail") String email,
-			Model model)
-	{
-		// This is now going to send the persons request with the value
-		model.addAttribute("name", name);
-		model.addAttribute("email", email);
-		return "index";
-	}
-	
-	
-	// This will handle post requests
-	@PostMapping("/")
-	public String loadIndexPost()
+	public String loadIndexGet()
 	{
 		return "index";
 	}
